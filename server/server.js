ead7199e3313537847b8751da8cc6b7dce4c3122
Arthur Baronov <abaronov1234@gmail.com>
2022-03-16 12:29:43 -0600

    Updating middleware

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const path = require("path");
+const exp = require('constants');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/styles", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-})
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
+
+// app.get("/styles", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// })
 
 const port = process.env.PORT || 4005;
 
